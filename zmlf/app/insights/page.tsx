9e982532ef553e56dcd8e331fcaea06db59edde5
Devulapellykushal <117680307+Devulapellykushal@users.noreply.github.com>
2025-04-10 18:36:14 +0530

    Update page.tsx

--- zmlf/app/insights/page.tsx
+++ zmlf/app/insights/page.tsx
@@ -44,7 +44,7 @@ export default function InsightsPage() {
     form.append('file', selectedFile);
 
     try {
-      const res = await fetch('http://127.0.0.1:5000/upload-csv/', {
+      const res = await fetch('https://zmlb.onrender.com/upload-csv/', {
         method: 'POST',
         body: form
       });
